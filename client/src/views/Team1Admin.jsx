fee0bae6cb1fdbe2d07dee4189bb861ca0befc1f
amiljour <austinmiljour@gmail.com>
2024-06-28 17:29:11 -0700

    Added Team Admin Pages

---
+++ client/src/views/Team1Admin.jsx
@@ -0,0 +1,9 @@
+
+
+const Team1Admin = () => {
+  return (
+    <div>Team1Admin</div>
+  )
+}
+
+export default Team1Admin
---
+++ client/src/views/Team2Admin.jsx
@@ -0,0 +1,9 @@
+
+
+const Team2Admin = () => {
+  return (
+    <div>Team2Admin</div>
+  )
+}
+
+export default Team2Admin
